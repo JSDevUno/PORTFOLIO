47b790264c00fc2282e77868692b5af01b8cbe05
Jeevy Seguerra <jeevyseguerra6@gmail.com>
2024-01-04 15:13:12 +0800

    change

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@ const preloader = document.querySelector(".preloader");
 window.addEventListener('load', () => {
     preloader.remove();
 });
+ const logo = document.querySelector(".logo");
+
+ logo.addEventListener('mouseover', ()=>{
+    logo.style.color="red";
+    logo.style.transition=".3s";
+ });
